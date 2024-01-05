d1c43bf0bdcbe20cd7ad9251a0a61dc408addea5
ArmanLK <74207911+ArmanLK@users.noreply.github.com>
2024-01-05 17:12:32 +0330

    are we testing?

--- test/parser_test.py
+++ test/parser_test.py
@@ -1,10 +1,11 @@
 from unittest import TestCase, main
 from lexer import LEXER
 
-from parser import PARSER
+from parser import PARSER, MySyntaxError
 
 
 class ParserTest(TestCase):
+
     def test1(self):
         test_input: str = """
         program prg1;
@@ -23,29 +24,30 @@ class ParserTest(TestCase):
         if divisor * quotient=num then
         return false;
         divisor:=divisor+1;
-        end;
+        end
         return true;
-        end;
+        end
         """
-        # TODO: do the check
         PARSER.parse(test_input, LEXER)
 
     def test2(self):
-        test_input = """program prg2;
-        function avg(integer m; integer n):real
+        test_input = """
+        program prg2;
+        function avg(integer m; integer n;):real
         integer sum, num;
         real average;
         begin
         sum:=0;
-        average:=0.0;
+        average:=0;
         for num:=m to n do
         sum:=sum+num;
         average:=sum/(n-m+1);
         return average;
-        end;
+        end
         begin
         a:=avg(1,20);
-        end;"""
+        end
+        """
         PARSER.parse(test_input, LEXER)
 
     def test_syntax_error_not_EOF(self):
@@ -54,17 +56,14 @@ class ParserTest(TestCase):
         begin
         if num = 1 return false;
         return true;
-        end;
+        end
         """
-        PARSER.parse(test_input, LEXER)
-
-    def test_syntax_error_EOF(self):  # TODO: this is probably not correct.
-        test_input = """program badTestEOF;
-        integer x,y;
-        begin
-        return 1 + 2 - 4 * 100;
-        end"""
-        PARSER.parse(test_input, LEXER)
+        try:
+            PARSER.parse(test_input, LEXER)
+        except MySyntaxError:
+            return
+        else:
+            raise
 
 
 if __name__ == "__main__":
